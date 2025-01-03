d7df462e04e09f35f5ebf52e13af063e37220627
unknown <ozzen0001@gmail.com>
2025-01-03 09:19:07 +0100

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-import { ThreeDCardDemo } from './Card';
+import { FloatingDock } from '@/components/ui/floating-dock';
 
+function A() {
+  return <h1>AA</h1>
+}
+function B() {
+  return <h1>BB</h1>
+}
+function C() {
+  return <h1>CC</h1>
+}
 export default function Home() {
+  const items = [
+    { title: 'a', icon: <A />, href: '' },
+    { title: 'b', icon: <B />, href: '' },
+    { title: 'c', icon: <C />, href: '' },
+  ];
   return (
-    <ThreeDCardDemo />
+    <div style={{margin:'10em'}}>
+    <FloatingDock items={items} />
+    </div>
   );
 }
